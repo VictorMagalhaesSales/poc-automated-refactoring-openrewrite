9ffec3e3b99da24e5bbb825c8f82080ea97caa58
Victor Magalhaes Sales <victor.sales@totvs.com.br>
2024-07-13 14:41:04 -0300

    Create a simple recipe to print app method names

---
+++ src/main/java/auto/code/refactoring/MyRecipe.java
@@ -0,0 +1,32 @@
+package auto.code.refactoring;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.J;
+
+public class MyRecipe extends Recipe {
+
+	@Override
+	public String getDisplayName() {
+		return "My recipe";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Recipe built for studies";
+	}
+
+	@Override
+	public TreeVisitor<?, ExecutionContext> getVisitor() {
+		return new JavaIsoVisitor<>() {
+			@Override
+			public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext p) {
+				System.out.println(method.getSimpleName());
+				return super.visitMethodDeclaration(method, p);
+			};
+		};
+	}
+
+}
